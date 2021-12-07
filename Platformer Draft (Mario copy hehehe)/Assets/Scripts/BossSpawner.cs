79c19710fb146b0315200d96f51a5c55fb06ee92
bw55555 <brianwang2013@gmail.com>
2021-12-07 16:51:36 -0500

    Add boss spawning

---
+++ Platformer Draft (Mario copy hehehe)/Assets/Scripts/BossSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject boss;
+    public GameObject bossLevelSlider;
+    public GameObject bossHealthBar;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D()
+    {
+        bossLevelSlider.SetActive(true);
+        bossHealthBar.SetActive(true);
+        boss.SetActive(true);
+    }
+}
