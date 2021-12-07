9df653bccdc99b88ae77f4351c77c03bc82e1478
bw55555 <brianwang2013@gmail.com>
2021-12-07 14:33:19 -0500

    Added a level slider

---
+++ Platformer Draft (Mario copy hehehe)/Assets/Scripts/LevelSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform startPos;
+    public Transform endPos;
+    public Transform track;
+    public Slider slider;
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float prop = (track.position.x - startPos.position.x) / (endPos.position.x - startPos.position.x);
+        slider.value = Mathf.Clamp(prop, 0, 1);
+    }
+}
