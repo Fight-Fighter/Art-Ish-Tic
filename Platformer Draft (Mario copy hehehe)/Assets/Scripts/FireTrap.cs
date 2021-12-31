541b1e5abbb2a2d823236e12b1a82c088808dc51
Arjun Pandher <42449099+ArjunPandher@users.noreply.github.com>
2021-12-31 01:54:52 -0500

    firetrap and spike trap first attempt

---
+++ Platformer Draft (Mario copy hehehe)/Assets/Scripts/FireTrap.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firetrap : MonoBehaviour
+{
+    public float damage;
+
+    [Header("Firetrap Timers")]
+    public float activationDelay;
+    public float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered; //when the trap gets triggered
+    private bool active; //when the trap is active and can hurt the player
+
+    private Player player; // REPLACE WITH WHATEVER OBJECT YOUR DAMAGE METHOD IS IN
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered!");
+        if (collision.tag == "Player")
+        {
+            if (!triggered)
+            {
+                StartCoroutine(ActivateFiretrap());
+            }
+
+            // SET PLAYER HERE
+            // player = collision.GetComponent<Health>();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player = null;
+    }
+
+    private void Update()
+    {
+        if (active && player != null)
+            // add damage script here
+            Debug.Log("Damage!");
+    }
+
+    private IEnumerator ActivateFiretrap()
+    {
+        //turn the sprite red to notify the player and trigger the trap
+        triggered = true;
+        spriteRend.color = Color.red;
+
+        //Wait for delay, activate trap, turn on animation, return color back to normal
+        yield return new WaitForSeconds(activationDelay);
+        spriteRend.color = Color.white; //turn the sprite back to its initial color
+        active = true;
+        anim.SetBool("activated", true);
+
+        //Wait until X seconds, deactivate trap and reset all variables and animator
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
+        anim.SetBool("activated", false);
+    }
+}
---
+++ Platformer Draft (Mario copy hehehe)/Assets/Scripts/TrapDamage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TrapDamage : MonoBehaviour
+{
+    public int damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player p = collision.gameObject.GetComponent<Player>();
+        if (p != null)
+        {
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.rockSmash);
+            p.TakeDamage(1);
+        }
+    }
+}
